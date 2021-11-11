29d8d8e1ebcd386df2df9b73400c1eeeeb6c94e2
Nimith-M-Gowda <nimithbbz10@gmail.com>
2021-11-11 22:31:54 +0530

    refactoring routres

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ app.get('/', (req, res) => {
     .json({ message: 'Hello from the server side', app: 'Natour' });
 });
 
-app.get('/api/v1/tours', (req, res) => {
+const getalltours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     tours,
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const addtours = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -36,25 +36,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const newid = req.params.id * 1;
-  const newtour = tours.find(({ id }) => id === newid);
-
-  if (!newtour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id found',
-    });
-  }
-  return res.status(200).json({
-    status: 'success',
-    message: newtour,
-  });
-});
-
-app.patch('/api/v1/tours/:id', async (req, res) => {
+const updatetour = async (req, res) => {
   const gettour = tours.find(({ id }) => id === req.params.id * 1);
 
   const objectIndex = tours.findIndex((obj) => obj.id === req.params.id * 1);
@@ -80,14 +64,10 @@ app.patch('/api/v1/tours/:id', async (req, res) => {
       });
     }
   );
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deletetour = (req, res) => {
   const tourindex = tours.findIndex(({ id }) => id === req.params.id * 1);
-  console.log(
-    '🚀 ~ file: index.js ~ line 87 ~ app.delete ~ tourindex',
-    tourindex
-  );
 
   const deletedtour = tours.splice(tourindex, 1);
 
@@ -108,7 +88,29 @@ app.delete('/api/v1/tours/:id', (req, res) => {
       });
     }
   );
-});
+};
+const gettour = (req, res) => {
+  const newid = req.params.id * 1;
+  const newtour = tours.find(({ id }) => id === newid);
+
+  if (!newtour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id found',
+    });
+  }
+  return res.status(200).json({
+    status: 'success',
+    message: newtour,
+  });
+};
+
+app.route('/api/v1/tours').get(getalltours).post(addtours);
+app
+  .route('/api/v1/tours/:id')
+  .get(gettour)
+  .patch(updatetour)
+  .delete(deletetour);
 
 const port = '8000';
 
